912764f0d99c8fb49a572cbd8de7fb00a816c264
DerekPeacock <derek.peacock@yahoo.com>
2020-02-10 14:02:45 +0000

    error message cleared

--- Week2.php
+++ Week2.php
@@ -1,9 +1,10 @@
 <?php
-include "Student.php";
+
+    include "Student.php";
+
     $grades[0] = "Fail";
     $grades[1] = "III";
     $grades[2] = "II-2";
 
     echo "My Grade = " . $grades[1];
-
 ?>
